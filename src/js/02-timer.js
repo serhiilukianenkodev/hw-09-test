f9ce9362383d26bf712fecea9510871a16f9e744
serhiilukianenkodev <serhii.lukianenko.dev@gmail.com>
2024-07-23 23:54:52 +0300

    update function splitTime

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -62,20 +62,15 @@ function fillTime({ days = 0, hours = 0, mins = 0, secs = 0 } = {}) {
 }
 
 function splitTime(ms) {
-  const days = Math.floor(ms / (24 * 60 * 60 * 1000));
-  const hours = Math.floor(
-    (ms - days * 24 * 60 * 60 * 1000) / (60 * 60 * 1000)
-  );
-  const mins = Math.floor(
-    (ms - days * 24 * 60 * 60 * 1000 - hours * 60 * 60 * 1000) / (60 * 1000)
-  );
-  const secs = Math.floor(
-    (ms -
-      days * 24 * 60 * 60 * 1000 -
-      hours * 60 * 60 * 1000 -
-      mins * 60 * 1000) /
-      1000
-  );
+  const day = 24 * 60 * 60 * 1000;
+  const hour = 60 * 60 * 1000;
+  const min = 60 * 1000;
+  const sec = 1000;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms - days * day) / hour);
+  const mins = Math.floor((ms - days * day - hours * hour) / min);
+  const secs = Math.floor((ms - days * day - hours * hour - mins * min) / sec);
 
   return { days, hours, mins, secs };
 }
