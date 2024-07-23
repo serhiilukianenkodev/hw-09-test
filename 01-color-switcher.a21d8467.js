4922692573c4e5b9796cc80c373e24bca94af722
serhiilukianenkodev <99186322+serhiilukianenkodev@users.noreply.github.com>
2024-07-23 18:02:10 +0000

    Deploying to gh-pages from @ serhiilukianenkodev/hw-09-test@2546b5bd4fce245b22bf524e91a3e379d6a74617 🚀

---
+++ 01-color-switcher.a21d8467.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");t.addEventListener("click",(function(e){o=setInterval((()=>{n.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`,t.disabled=!0}),1e3)})),e.addEventListener("click",(function(e){clearInterval(o),t.disabled=!1}));let o=null;
+//# sourceMappingURL=01-color-switcher.a21d8467.js.map
