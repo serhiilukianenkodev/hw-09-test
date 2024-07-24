e062b25e55347f7c8a083e77d5707652ff709a29
serhiilukianenkodev <serhii.lukianenko.dev@gmail.com>
2024-07-24 12:29:14 +0300

    task 3 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  const delay = Number(evt.currentTarget.elements.delay.value);
+  const step = Number(evt.currentTarget.elements.step.value);
+  const amount = Number(evt.currentTarget.elements.amount.value);
+
+  for (let i = 0; i < amount; i += 1) {
+    const currentDelay = delay + step * i;
+    setTimeout(() => {
+      createPromise(i + 1, currentDelay)
+        .then(({ position, delay }) => {
+          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        })
+        .catch(({ position, delay }) => {
+          Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        });
+    }, currentDelay);
+  }
+
+  setTimeout(() => {
+    evt.target.reset();
+  }, delay + step * amount);
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      resolve({ position, delay });
+    } else {
+      reject({ position, delay });
+    }
+  });
 }
